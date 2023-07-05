7da590d867b3b72ab7b4c63b9b81d85e6c8cd8df
Sodalicus <soda@pipboy2000>
2023-07-05 16:27:03 +0200

    poll - no progress, haven't had any time

--- flask/poll.py
+++ flask/poll.py
@@ -10,9 +10,21 @@
 Simple poll app
 """
 
-from flask import Flask
+from flask import Flask, render_template
+
+poll = [{"question" : "
+        {"choice" ; "choice 1", "count" : 0 },
+        {"choice" : "choice 2", "count" : 0 },
+        {"choice" : "choice 3", "count" : 0 }]
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST", "GET"]
+def index():
+    return render_template("poll.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
